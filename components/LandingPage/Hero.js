c5ec2fcace6493cb0627a33997168a6314998d50
Jeffrey Gao <jpg6424@psu.edu>
2026-03-04 15:50:58 -0500

    Modify Hero.js by making a more stylized hero image. This change was made for the checkpoint, but I forgot to commit and push this modification.

--- components/LandingPage/Hero.js
+++ components/LandingPage/Hero.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Hero = ({text}) => {
+const Hero = ({/*text*/}) => { //removed this text as of right now
   return (
     <Section>
       <Overlay>
+        <Glow />
         <Container>
           <HeroTextColumn>
-            <Header>
-              {text}
-              <Highlight>Start Here</Highlight>
-            </Header>
+            <Header>Welcome to MoreFlicks!</Header>
             <SubheaderAndStarsColumn>
-              <SubHeader>Insert creative subheader here</SubHeader>
-              <CTAButton>Get Started</CTAButton>
+              <SubHeader>Spend more time watching, less time scrolling</SubHeader>
+              {/*<CTAButton>Get Started</CTAButton>.   Also CTA means call to action*/}
+
+              {/*Add a way to make slideshows for media scenes as offerings for different shows*/} 
             </SubheaderAndStarsColumn>
           </HeroTextColumn>
         </Container>
@@ -23,28 +23,49 @@ const Hero = ({text}) => {
 };
 
 const Section = styled.section`
-background-color: blue;
-width: 100%;
-height: 10vh;
+  position: relative;
+  width: 100%;
+  height: 50vh;
+  display: flex;
+  align-items: center;
+  background: #372b27ff;
+  overflow: hidden;
+
 `;
 
 const Overlay = styled.div`
+  
 `;
 
 const Container = styled.div`
+  width: 100%;
+  max-width: 1100px;
+  margin: 0 auto;
+  padding: 0 300px;
+  position: relative; /* This keeps text above the background glow */
+  z-index: 1; /* This makes sure that content stays above background effects */
 `;
 
 const HeroTextColumn = styled.div`
+  
 `;
 
 const Header = styled.h1`
+  color: #ffffff;
+  font-size: 44px;
+  line-height: 1.05;
+  font-weight: 800;
+  
+  
 `;
 
 const Highlight = styled.span`
 `;
 
 const SubHeader = styled.h2`
-
+  color: #a2a0a0ff;
+  align-items: center;
+  text-align: center;
 `;
 
 const SubheaderAndStarsColumn = styled.div`
@@ -54,5 +75,13 @@ const SubheaderAndStarsColumn = styled.div`
 const CTAButton = styled.button`
 
 `;
+
+const Glow = styled.div`
+  position: absolute;
+  inset: -200px;
+  background: radial-gradient(closest-side, rgba(255, 255, 255, 0.10), rgba(11, 15, 20, 0));
+  filter: blur(40px);
+  pointer-events: none;
+`
 
 export default Hero;
