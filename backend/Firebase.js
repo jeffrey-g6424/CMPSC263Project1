2ade3f7ca74afd2960f637c9e806855ac12baed9
Jeffrey Gao <jpg6424@psu.edu>
2026-03-04 15:36:43 -0500

    Modify Firebase.js with instructions given by the professor in class when setting up Firebase. Changes were made before the checkpoint, but I forgot to commit and push this modification.

--- backend/Firebase.js
+++ backend/Firebase.js
@@ -4,29 +4,34 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = process.env.NODE_ENV === 'production' ? {
-//   apiKey: "",
-//   authDomain: "",
-//   projectId: "",
-//   storageBucket: "",
-//   messagingSenderId: "",
-//   appId: "", 
-//   measurementId: ""
-// } : {
-//   apiKey: "",
-//   authDomain: "",
-//   projectId: "",
-//   storageBucket: "com",
-//   messagingSenderId: "",
-//   appId: "",
-//   measurementId: ""
-// }
+
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAmEUYPmoupWamUJAZiPfJMYgpPxK0Votw",
+  authDomain: "project1-a5d63.firebaseapp.com",
+  projectId: "project1-a5d63",
+  storageBucket: "project1-a5d63.firebasestorage.app",
+  messagingSenderId: "452448420291",
+  appId: "1:452448420291:web:808228336ef82d0a9abd76",
+  measurementId: "G-NVS3END2XL"
+};
+
+// Initialize Firebase
+// const analytics = getAnalytics(app);
+
 
 // // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
+ const app = initializeApp(firebaseConfig);
+ export const auth = getAuth(app);
 // export const storage = getStorage(app);
-// export const database = getFirestore(app);
+ export const database = getFirestore(app);
 // export const analytics = () => getAnalytics(app);
 
-// export default app
+ export default app
