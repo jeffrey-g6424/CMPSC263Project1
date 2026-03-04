ff1316a507f6a77eeb1b81fa0ab0ed7c07f93f06
Jeffrey Gao <jpg6424@psu.edu>
2026-03-04 15:35:29 -0500

    Modify Database.js by using functions given from Firebase functions on Canvas. Changes were made before the checkpoint, but I forgot to commit and push this modification.

--- backend/Database.js
+++ backend/Database.js
@@ -1,4 +1,16 @@
 import { doc, setDoc, getDoc, getDocs, collection, query, where } from "firebase/firestore"
 import { database } from "./Firebase"
 
+// Create document (auto ID)
+export async function createDocument(collectionName, data) {
+  const docRef = await addDoc(collection(database, collectionName), data);
+  return docRef.id;
+}
+//Usage: const id = await createDocument("users", { name: "Bob", age: 22 });
 
+// Create / overwrite document with custom ID
+export async function setDocument(collectionName, docId, data) {
+  await setDoc(doc(database, collectionName, docId), data);
+  return docId;
+}
+//Usage: await setDocument("users", "user123", { name: "Bob" });
